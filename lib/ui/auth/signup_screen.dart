e561dedfb8d1230ee112626d5ed84801bb7ea76a
Hassan Ahmed <hassanahmed40005@gmail.com>
2023-06-01 17:22:09 +0500

    Initial Commit

--- lib/ui/auth/signup_screen.dart
+++ lib/ui/auth/signup_screen.dart
@@ -1,15 +1,14 @@
-import 'package:firebase_class/ui/auth/signup_screen.dart';
+import 'package:firebase_class/ui/auth/login_screen.dart';
 import 'package:firebase_class/widget/round_button.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({Key? key}) : super(key: key);
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  State<SignUpScreen> createState() => _LoginScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _LoginScreenState extends State<SignUpScreen> {
   final _formKey = GlobalKey<FormState>();
   final emailController = TextEditingController();
   final  passwordController = TextEditingController();
@@ -23,39 +22,33 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: ()async{
-        SystemNavigator.pop();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("SignUp Screen"),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
 
-        return true;
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Login Screen"),
-          centerTitle: true,
-          backgroundColor: Colors.deepPurple,
-        ),
-        body: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-
-              Form(
-                key: _formKey,
-                child: Column(
+            Form(
+              key: _formKey,
+              child: Column(
 
                 children: [
                   TextFormField(
                     controller: emailController,
                     decoration: InputDecoration(
                         hintText: "Email",
-                      prefixIcon: Icon(Icons.email)
+                        prefixIcon: Icon(Icons.email)
                     ),
                     validator: (value){
                       if(value!.isEmpty){
-                         return "Plz Enter the email";
+                        return "Plz Enter the email";
                       }else
                         return null;
                     },
@@ -77,27 +70,26 @@ class _LoginScreenState extends State<LoginScreen> {
                 ],
               ),
 
-              ),
+            ),
 
-              SizedBox(height: 50,),
-              RoundButton(title: "Log In",
-                onpress: (){
+            SizedBox(height: 50,),
+            RoundButton(title: "Sign In",
+              onpress: (){
                 if(_formKey.currentState!.validate()){
 
                 }
 
               },),
-              SizedBox(height: 20,),
-              Row(children: [
-                Text("Don't have an account"),
-                TextButton(onPressed: (){
+            SizedBox(height: 20,),
+            Row(children: [
+              Text("Already have an account"),
+              TextButton(onPressed: (){
 
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=>SignUpScreen()));
-                }, child: Text('SignUp'),)
-                  ],)
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginScreen()));
+              }, child: Text('Login'),)
+            ],)
 
-            ],
-          ),
+          ],
         ),
       ),
     );
